cbe444f1bf9ed7108c4c118fff716974ae6f279f
Navlok Mishra <navlok.mishra@fortanix.com>
2020-02-24 18:46:47 +0530

    Moving the input_vector to common crate

--- array-search/linear-search/src/main.rs
+++ array-search/linear-search/src/main.rs
@@ -1,24 +1,6 @@
+use common;
 use std::io;
 use std::io::Write;
-
-fn input_vector(number_of_elements: i32) -> Vec<i32> {
-	let mut arr: Vec<i32> = Vec::new();
-
-	for i in 1..number_of_elements+1 {
-		print!("Enter the value of element {:?}: ", i );
-		if let Err(error) = io::stdout().flush() {
-			panic!("{}", error);
-		}
-
-		let mut element = String::new();
-		io::stdin().read_line(&mut element).expect("Failed to read line");
-		let element = element.trim().parse::<i32>().unwrap();
-
-		arr.push(element);
-	}
-
-	return arr;
-}
 
 fn search(vec: & Vec<i32>, element_to_be_searched: i32) -> bool {
 	for elem in vec.iter() {
@@ -40,7 +22,7 @@ fn main() {
 	io::stdin().read_line(&mut var).expect("Failed to read line");
 	let var = var.trim().parse::<i32>().unwrap();
 
-	let arr = input_vector(var);
+	let arr = common::input_vector(var);
 
 	print!("Enter the element to be searched in the array: ");
 	if let Err(error) = io::stdout().flush() {
