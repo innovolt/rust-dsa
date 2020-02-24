19dea1ed90bc786966baf2062157d440f8832b9f
Navlok Mishra <navlok.mishra@fortanix.com>
2020-02-24 15:43:06 +0530

    Create a separate function which returns the vector. The plain is to move it common place so that anybody needs to input vector then they can use it

--- array-search/linear-search/src/main.rs
+++ array-search/linear-search/src/main.rs
@@ -1,48 +1,58 @@
 use std::io;
 use std::io::Write;
 
-fn search(vec: & Vec<i32>, element: i32) -> bool {
-	for elem in vec.iter() {
-		if element == *elem {
-			return true;
-		}
-	}
-
-	return false;
-}
-
-fn main() {
-	let mut num_of_elements: String = String::new();
+fn input_vector(number_of_elements: i32) -> Vec<i32> {
 	let mut arr: Vec<i32> = Vec::new();
 
-	print!("Enter number of elements in the array: ");
-	io::stdout().flush();
-
-	io::stdin().read_line(&mut num_of_elements).expect("Failed to read line");
-	let num_of_elements = num_of_elements.trim().parse::<i32>().unwrap();
-	let mut element: i32;
-
-	for i in 1..num_of_elements+1 {
+	for i in 1..number_of_elements+1 {
 		print!("Enter the value of element {:?}: ", i );
-		io::stdout().flush();
+		//io::stdout().flush()?;
+		if let Err(error) = io::stdout().flush() {
+			panic!("{}", error);
+		}
 
-		let mut num_of_elements = String::new();
-		io::stdin().read_line(&mut num_of_elements).expect("Failed to read line");
-		element = num_of_elements.trim().parse::<i32>().unwrap();
+		let mut element = String::new();
+		io::stdin().read_line(&mut element).expect("Failed to read line");
+		let element = element.trim().parse::<i32>().unwrap();
 
 		arr.push(element);
 	}
 
+	return arr;
+}
+
+fn search(vec: & Vec<i32>, element_to_be_searched: i32) -> bool {
+	for elem in vec.iter() {
+		if element_to_be_searched == *elem {
+			return true;
+		}
+	}
+	return false;
+}
+
+fn main() {
+	let mut var: String = String::new();
+
+	print!("Enter number of elements in the array: ");
+	if let Err(error) = io::stdout().flush() {
+		panic!("{}", error);
+	}
+
+	io::stdin().read_line(&mut var).expect("Failed to read line");
+	let var = var.trim().parse::<i32>().unwrap();
+
+	let arr = input_vector(var);
+
 	print!("Enter the element to be searched in the array: ");
-	io::stdout().flush();
+	if let Err(error) = io::stdout().flush() {
+		panic!("{}", error);
+	}
 
-	let mut element_to_be_searched = String::new();
-	io::stdin().read_line(&mut element_to_be_searched).expect("Failed to read line");
-	let element_to_be_searched = element_to_be_searched.trim().parse::<i32>().unwrap();
+	let mut var = String::new();
+	io::stdin().read_line(&mut var).expect("Failed to read line");
+	let var = var.trim().parse::<i32>().unwrap();
 
-	let mut found = search(&arr, element_to_be_searched);
-
-	if found == true {
+	if search(&arr, var) {
 		println!("Found");
 	} else {
 		print!("Not found");
